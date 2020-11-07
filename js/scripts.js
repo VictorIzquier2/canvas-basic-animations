7158368707a9f4a378b51bdc19db4f1ad66b4163
victorizquier2 <victor.izquierdo1985@gmail.com>
2020-11-07 18:52:39 +0100

    Game & obstacles

--- js/scripts.js
+++ js/scripts.js
@@ -116,27 +116,27 @@ const printCharacter = () => {
   const ghost = new Character();
 
   document.addEventListener('keydown', (e) => {
-    console.log(e);
+    //console.log(e);
     switch(e.keyCode){
       case 38:
         ghost.moveUp();
-        console.log('up', ghost);
+        //console.log('up', ghost);
         break;
       case 40:
         ghost.moveDown();
-        console.log('down', ghost);
+        //console.log('down', ghost);
         break;
       case 37:
         ghost.moveLeft();
-        console.log('right', ghost);
+        //console.log('right', ghost);
         break;
       case 39:
         ghost.moveRight();
-        console.log('right', ghost);
+        //console.log('right', ghost);
         break;
       case 32:
         ghost.x = 25; ghost.y = 25;
-        console.log('restart', ghost);
+        //console.log('restart', ghost);
         break;
     }
     updateCanvas4();
@@ -190,3 +190,136 @@ const updateCanvas5 = () => {
 
 };
 window.addEventListener('load', updateCanvas5);
+
+
+const startGame = () => {
+  class Component {
+    constructor(width, height, color, x, y){
+      this.width = width;
+      this.height = height;
+      this.color = color;
+      this.x = x;
+      this.y = y;
+      this.speedX = 0;
+      this.speedY = 0;
+    }
+    newPos(){
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
+    update(){
+      const ctx6 = myGameArea.context;
+      ctx6.fillStyle = this.color;
+      ctx6.fillRect(this.x, this.y, this.width, this.height);
+      ctx6.fillStyle = 'black';
+      ctx6.font = '1rem serif';
+      ctx6.fillText(player.x + 'x', 260, 20);
+      ctx6.fillText(player.y + 'y', 260, 40);
+    }
+    left(){
+      return this.x;
+    }
+    right(){
+      return this.x + this.width;
+    }
+    top(){
+      return this.y;
+    }
+    bottom(){
+      return this.y + this.height;
+    }
+    crashWith(obstacle){
+      return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
+    }
+  }
+
+  const obstacles = [];
+
+  const myGameArea = {
+    canvas: document.getElementById('canvas6'),
+    frames: 0,
+    start: function() {
+      this.canvas.width = 300;
+      this.canvas.height = 300,
+      this.context = this.canvas.getContext('2d');
+      this.interval = setInterval(updateGameArea, 40);
+    },
+    stop: function(){
+      clearInterval(this.interval);
+    },
+    clear: function() {
+      this.context.clearRect(0, 0, 300, 300);
+    },
+    score: function(){
+      const points = Math.floor(this.frames / 60);
+      this.context.font = '1rem serif';
+      this.context.fillStyle = 'navy';
+      this.context.fillText(`Score: ${points}`, 20, 20);
+    }
+  };
+
+  const player = new Component(30, 30, 'red', 10, 110);
+  
+  const updateObstacles = () => {
+    
+    myGameArea.frames += 1;
+    if(myGameArea.frames % 60 === 0) {
+      let x = myGameArea.canvas.width;
+      let minHeight = 20;
+      let maxHeight = 120;
+      let height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+      let minGap = 50;
+      let maxGap = 180;
+      let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+      obstacles.push(new Component(10, height, 'green', x, 0));
+      obstacles.push(new Component(10, x - height - gap, 'green', x, height + gap));
+    }
+    for(i = 0; i < obstacles.length; i++){
+      obstacles[i].x += -1;
+      obstacles[i].update();
+    }
+  };
+  
+  const checkGameOver = () => {
+    const crashed = obstacles.some(function(obstacle) {
+      return player.crashWith(obstacle);
+    });
+    if(crashed){
+      myGameArea.stop();
+    }
+  };
+
+  const updateGameArea = () => {
+    myGameArea.clear();
+    player.newPos();
+    player.update();
+    updateObstacles();
+    checkGameOver();
+    myGameArea.score();
+  };
+
+  document.addEventListener('keydown', (e) =>{
+    console.log(e)
+    switch(e.keyCode){
+      case 87:
+        player.speedY = -5;
+        break;
+      case 83:
+        player.speedY = 5;
+        break;
+      case 65:
+        player.speedX = -5;
+        break;
+      case 68:
+        player.speedX = 5;
+        break;
+    }
+  });
+  document.addEventListener('keyup', (e) =>{
+    player.speedX = 0;
+    player.speedY = 0;
+  });
+  myGameArea.start();
+  
+};
+window.addEventListener('load', startGame); 
